2373d0cfff11a2eeebdec3cc200aa8369305694d
Edward <edward.debrouwer@gmail.com>
2017-10-24 16:24:54 +0200

    Modified test script

--- chembl_test.py
+++ chembl_test.py
@@ -13,23 +13,25 @@ alpha=5.0
 burn=400
 samples=1600
 
-df=pd.read_csv("./outputs/CHEM23/CHEM23_pic50_cens.csv")
+df=pd.read_csv("./outputs/CHEM23/CHEM23_pic50_cens.csv").dropna()
 
-df["test"]=0
-df.loc[df["cens"]==0].sample(frac=0.9)["test"]=1
+#df["test"]=0
+#df.loc[df["cens"]==0].sample(frac=0.9)["test"]=1
 df.at[random.sample(df.loc[df["cens"]==0].index,int(test_prop*df.shape[0])),"test"]=1
 print(df.head())
 
 #Test flag
-Test=df.pivot(index='cmpd_id',columns='target_id',values='test').fillna(0).astype(bool)
+Test=df.pivot(index='cmpd_id',columns='target_id',values='test').fillna(0).astype(bool).as_matrix()
 
 #Activations
-D=df[["cmpd_id","target_id","pic50_cens"]]
+#D=df[["cmpd_id","target_id","pic50_cens"]]
 
-print(D.head())
-Dmat=D.pivot(index='cmpd_id',columns='target_id',values='pic50_cens').fillna(0)
+#print(D.head())
+Dmat=df.pivot(index='cmpd_id',columns='target_id',values='pic50_cens').fillna(0).as_matrix()
 #print(D.loc[D["pic50_cens"]==0])
-Dtest=Dmat[Test].fillna(0)
+Dtest=np.zeros(Dmat.shape)
+Dtest[Test]=Dmat[Test]
+#Dtest=Dmat[Test].fillna(0)
 Dmat[Test]=0
 
 
@@ -44,9 +46,11 @@ Cens=df[["cmpd_id","target_id","cens"]]
 Cmat=Cens.pivot(index='cmpd_id',columns='target_id',values='cens').fillna(0)
 C=scipy.sparse.coo_matrix(Cmat)
 
+
 #
-Dmat[Cmat.astype(bool)]=0
+Dmat[Cmat.astype(bool).as_matrix()]=0
 Y_nocens=scipy.sparse.coo_matrix(Dmat)
+
 
 #Side information
 ecfp=scipy.io.mmread("./outputs/CHEM23/ecfp.mtx")
