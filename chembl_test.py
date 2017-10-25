bc8b3008f91363ee9134a82a6b8f9f58435574a4
Edward <edward.debrouwer@gmail.com>
2017-10-25 13:52:02 +0200

    Changed path for ecfp

--- chembl_test.py
+++ chembl_test.py
@@ -53,7 +53,7 @@ Y_nocens=scipy.sparse.coo_matrix(Dmat)
 
 
 #Side information
-ecfp=scipy.io.mmread("./outputs/chembl_23/ecfp.mtx")
+ecfp=scipy.io.mmread("./outputs/chembl_23/chembl_23/ecfp.mtx")
 result=macau.macau(Y=Y,Ytest=Ytest,side=[ecfp,None], num_latent=latents,precision=alpha,burnin=burn,nsamples=samples)
 result2=macau.macau(Y=Y_nocens,Ytest=Ytest,side=[ecfp,None],num_latent=latents,precision=alpha,burnin=burn,nsamples=samples)
 
